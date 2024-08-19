516148bf4984990e9f2517966329d8eeae01e41e
Mahesh Langote <88247028+Mahesh-Langote@users.noreply.github.com>
2024-08-19 23:29:57 +0530

    Added / route

--- src/app.js
+++ src/app.js
@@ -12,6 +12,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use('server is on');
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/api/auth', authRoutes);
 app.use('/api/posts', postRoutes);
